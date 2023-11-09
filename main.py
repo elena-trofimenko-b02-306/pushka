172140e3cdaa2f299445f9cfecec46a612bdafc0
lena <trofimenkolena25@gmail.com>
2023-11-09 16:44:06 +0500

    я нарисовала фигуры 1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = pg.display.set_mode((800,800))
 r = pg.Rect(300,300,100,200)
 pg.draw.rect(screen,(0,0,100),r,60)
 pg.draw.line(screen,(240,0,100),(0,0),(100,300),3)
-pg.draw.circle(screen,(0,0,250),(300,200),40,5)
+pg.draw.circle(screen,(0,100,250),(300,200),40,5)
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
